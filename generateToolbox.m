928a89418f4bef75c48eca7729b992bd61d477f3
Rodrigo <rodrigocgob@gmail.com>
2023-07-28 17:22:22 +0200

    Minor format changes

--- generateToolbox.m
+++ generateToolbox.m
@@ -10,7 +10,7 @@ identifier = "bossdevice-api-matlab";
 opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder, identifier);
 
 opts.ToolboxName = "Bossdevice API Toolbox";
-opts.ToolboxVersion = version; % Should be set by Github action
+opts.ToolboxVersion = version;
 opts.Summary = "sync2brain's bossdevice RESEARCH Application Programmable Interface (API) for MATLAB."; 
 opts.Description = "For a more detailed description refer to the toolbox README.md file. Contact email: support@sync2brain.com";
 opts.AuthorName = "sync2brain";
@@ -30,7 +30,7 @@ opts.SupportedPlatforms.MatlabOnline = true;
 opts.SupportedPlatforms.Win64 = true;
 %opts.ToolboxJavaPath = ""; %Not applicable
 
-% %Required Add-ons (TO BE CONFIRMED)
+% %Required Add-ons (TO BE COMPLETED)
 % opts.RequiredAddons = ...
 %     struct("Name","Embedded Coder", ...
 %            "Identifier","xxx", ...   % TBC Identifier missing
@@ -38,20 +38,15 @@ opts.SupportedPlatforms.Win64 = true;
 %            "LatestVersion","7.10", ...
 %            "DownloadURL","https://www.mathworks.com/products/embedded-coder.html");
 % 
-% opts.RequiredAddons = ...
-%     struct("Name","Simulink Desktop Real-Time", ...
-%            "Identifier","xxx", ...  % TBC Identifier missing
-%            "EarliestVersion","5.0", ...
-%            "LatestVersion","5.16", ...
-%            "DownloadURL","https://www.mathworks.com/products/simulink-desktop-real-time.html");
+% Simulink Desktop Real-Time
 
 % Required Additional Software 
 % --> Not Applicable
 
-toolboxOptions = opts;
-
 % Generate toolbox
 matlab.addons.toolbox.packageToolbox(opts);
+
+toolboxOptions = opts;
 
 end
 
