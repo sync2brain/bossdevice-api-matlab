3c0dba018b70aec4f0f4ce6e694923f10d283b29
Pablo Romero <pablorcum@gmail.com>
2023-08-06 18:26:09 +0200

    Analyze code issues from the root project folder. Minor fixes.

--- buildUtilities/checkTask.m
+++ buildUtilities/checkTask.m
@@ -1,7 +1,9 @@
-function checkTask(~)
-% Identify code issues
-        issues = codeIssues('../toolbox');
-        assert(isempty(issues.Issues),formattedDisplayText( ...
-            issues.Issues(:,["Location" "Severity" "Description"])))
-end
+function issues = checkTask
 
+projObj = currentProject;
+
+% Identify code issues
+issues = codeIssues(projObj.RootFolder);
+assert(isempty(issues.Issues),...
+    formattedDisplayText(issues.Issues(:,["Location" "Severity" "Description"])));
+end
