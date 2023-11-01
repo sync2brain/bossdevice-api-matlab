9cf9ab0d47f8ad0aebbb6509888baa43c7031e78
Pablo Romero <pablorcum@gmail.com>
2023-11-02 00:37:17 +0100

    Fix comparing different signals with linear interpolation.

--- toolbox/examples/demo_phase_prediction_error_simple.m
+++ toolbox/examples/demo_phase_prediction_error_simple.m
@@ -40,8 +40,8 @@ disp('Done.');
 
 sigData = mapData.values;
 
-spf_sigData = squeeze(sigData{2}.data);
-osc_alpha_ipData = squeeze(sigData{1}.data);
+spf_sigData = squeeze(sigData{2}.data)';
+osc_alpha_ipData = interp1(sigData{1}.time,sigData{1}.data,sigData{2}.time);
 
 
 %% Phase error using standard non-causal methods
@@ -54,7 +54,7 @@ fs = 1/mean(diff(sigData{1}.time));
 PhaseErrorFilter = designfilt('bandpassfir', 'FilterOrder', round(fs), 'CutoffFrequency1', 9, 'CutoffFrequency2', 13, 'SampleRate', fs);
 
 % Compute phase prediction error
-[phaseError, meanError, meanDev] = bossapi.computePhasePredictionError(PhaseErrorFilter, spf_sigData(1,:), osc_alpha_ipData(1,:));
+[phaseError, meanError, meanDev] = bossapi.computePhasePredictionError(PhaseErrorFilter, spf_sigData(:,1), osc_alpha_ipData(:,1));
 
 disp('Done.');
 
