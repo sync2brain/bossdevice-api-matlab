69f9d11e52634234cc0f2ad47a97ea3c120c77df
Pablo Romero <pablo.romero@sync2brain.com>
2024-02-28 00:11:00 +0100

    Fix comment

--- toolbox/src/bossdevice.m
+++ toolbox/src/bossdevice.m
@@ -2,7 +2,6 @@ classdef bossdevice < handle
     %BOSSDEVICE Application Programming Interface
     %   API to control the bossdevice from Matlab
     %   Requires Simulink Real-Time toolbox
-    %   Supported for Matlab version 2023a
 
     properties
         theta bossdevice_oscillation
