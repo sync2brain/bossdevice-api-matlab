1944757fd8ef83f7b526845e12badbc3511e9e78
Pablo Romero <pablorcum@gmail.com>
2024-02-27 21:35:45 +0100

    Improve test when the bossdevice had been in use.

--- tests/basicTests.m
+++ tests/basicTests.m
@@ -10,6 +10,9 @@ classdef basicTests < matlab.unittest.TestCase
         % Test methods that do not require bossdevice or any target connected
         function noBossdevice(testCase)
             testCase.bd = bossdevice;
+            if testCase.bd.isConnected
+                testCase.bd.delete;
+            end
             testCase.verifyFalse(testCase.bd.isConnected);
         end
     end
