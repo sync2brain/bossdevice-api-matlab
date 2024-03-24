49ef661793d5c29062407853719b8d9797fb6939
Pablo Romero <pablorcum@gmail.com>
2024-03-24 06:53:59 +0100

    Fix outputFolder.

--- buildUtilities/buildDoc.m
+++ buildUtilities/buildDoc.m
@@ -3,8 +3,10 @@ function buildDoc
 % Get current project object
 projObj = currentProject;
 
+outputFolder = fullfile(projObj.RootFolder,'toolbox','html');
+
 % Export documentation to HTML before packaging toolbox
-bossapi.exportToHTML(fullfile(projObj.RootFolder,'docSource'),fullfile(projObj.RootFolder,'toolbox','html'));
+bossapi.exportToHTML(fullfile(projObj.RootFolder,'docSource'),outputFolder);
 
 % Build searchable database
 builddocsearchdb(outputFolder);
