344e6852874b81997d66a663da5592d57ebb3292
Pablo Romero <pablo.romero@sync2brain.com>
2025-11-16 20:42:25 +0100

    Update firmware download link in error message

--- toolbox/src/bossdevice.m
+++ toolbox/src/bossdevice.m
@@ -194,7 +194,7 @@ classdef bossdevice < handle
             [filename, filepath] = uigetfile([obj.appName,'.mldatx'],...
                 'Select the firmware binary to load on the bossdevice');
             if isequal(filename,0)
-                obj.logObj.error('User selected Cancel. Please download the latest firmware version from <a href="https://sync2brain.com/downloads-bossdevice-research">sync2brain downloads portal</a> and select the firmware mldatx file to complete bossdevice dependencies.');
+                obj.logObj.error('User selected Cancel. Please download the latest firmware version from <a href="https://sync2brain.com/bossdevice-research-downloads">sync2brain downloads portal</a> and select the firmware mldatx file to complete bossdevice dependencies.');
             else
                 obj.firmwareFilepath = fullfile(filepath,filename);
             end
